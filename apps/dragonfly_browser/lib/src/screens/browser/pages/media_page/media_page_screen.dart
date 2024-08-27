dcb34f1664d101127b7470c11dad7d9ed341cd0e
Clement <clement.beal@outlook.com>
2024-08-27 18:02:56 -0400

    can scroll to the click position for an image

--- apps/dragonfly_browser/lib/src/screens/browser/pages/media_page/media_page_screen.dart
+++ apps/dragonfly_browser/lib/src/screens/browser/pages/media_page/media_page_screen.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
-
-import 'package:dragonfly_navigation/dragonfly_navigation.dart';
 import 'package:flutter/material.dart';
+import 'package:dragonfly_navigation/dragonfly_navigation.dart';
 
 class MediaPageScreen extends StatelessWidget {
   const MediaPageScreen({super.key, required this.page});
@@ -11,7 +10,7 @@ class MediaPageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DecoratedBox(
-      decoration: BoxDecoration(
+      decoration: const BoxDecoration(
         color: Colors.black87,
       ),
       child: ImageMediaPage(
@@ -31,11 +30,67 @@ class ImageMediaPage extends StatefulWidget {
 }
 
 class _ImageMediaPageState extends State<ImageMediaPage> {
+  bool isZooming = false;
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _zoomAtPosition(TapUpDetails details) {
+    final tapPosition = details.localPosition;
+
+    final newScrollY =
+        (tapPosition.dy * _scrollController.position.maxScrollExtent) /
+            context.size!.height;
+
+    _scrollController.jumpTo(newScrollY);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Image.file(
-      File(widget.page.url),
-      fit: BoxFit.fitHeight,
+    return MouseRegion(
+      cursor:
+          isZooming ? SystemMouseCursors.zoomOut : SystemMouseCursors.zoomIn,
+      child: GestureDetector(
+        onTapUp: (details) {
+          setState(() {
+            isZooming = !isZooming;
+          });
+
+          if (isZooming) {
+            WidgetsBinding.instance.addPostFrameCallback(
+              (timeStamp) {
+                _zoomAtPosition(details);
+              },
+            );
+          }
+        },
+        child: (isZooming)
+            ? SingleChildScrollView(
+                controller: _scrollController,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.file(
+                      File(widget.page.url),
+                    ),
+                  ],
+                ),
+              )
+            : Image.file(
+                File(widget.page.url),
+              ),
+      ),
     );
   }
 }
