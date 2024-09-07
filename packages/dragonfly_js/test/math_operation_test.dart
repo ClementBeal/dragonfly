7d9e8ea3789bd7ed5ead3d61245707861709316e
Clement <clement.beal@outlook.com>
2024-09-06 23:45:55 -0400

    better tests

--- packages/dragonfly_js/test/math_operation_test.dart
+++ packages/dragonfly_js/test/math_operation_test.dart
@@ -5,7 +5,7 @@ void main() {
   group(
     'Math operations',
     () {
-      group('Addition', () {
+      group('Simple numbers', () {
         final additionData = [0, 22, 8239, 182930293, -2, -8, -18293];
 
         for (var data in additionData) {
@@ -22,25 +22,29 @@ void main() {
 
       group('Addition', () {
         final additionData = [
-          (2, 3, 5),
-          (0, 5, 5),
-          (2, 3, 5),
-          (523, 3, 526),
-          (222, 222, 444),
-          // Decimal integers:
-          (2.3, 3, 5.3),
-          (2.3, 3.5, 5.8),
-          (10.3, 3.5, 13.8),
+          ('2 + 3', 5),
+          ('0 + 5', 5),
+          ('2 + 3', 5),
+          ('523 + 3', 526),
+          ('222 + 222', 444),
+          ('-2 + -3', -5),
+          ('-5 + -10', -15),
+          ('-2 + 3', 1),
+          ('5 + -7', -2),
+          ('0 + -8', -8),
+          ('2.3 + 3', 5.3),
+          ('2.3 + 3.5', 5.8),
+          ('10.3 + 3.5', 13.8),
         ];
 
         for (var data in additionData) {
-          test('${data.$1} + ${data.$2}  = ${data.$3}', () {
+          test('${data.$1} = ${data.$2}', () {
             final interpreter = JavascriptInterpreter();
 
-            final code = "${data.$1} + ${data.$2}";
+            final code = data.$1;
             final result = interpreter.execute(code);
 
-            expect(data.$3, equals(result));
+            expect(data.$2, equals(result));
           });
         }
       });
