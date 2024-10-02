c3e73b8f7c5f7d3a025e8d0c8d31d2497cd7532a
Clement <clement.beal@outlook.com>
2024-10-02 15:01:43 -0300

    good inspector layout tool

--- apps/dragonfly_browser/lib/src/screens/developer_tools/tools/inspector/tools/inspector_layout.dart
+++ apps/dragonfly_browser/lib/src/screens/developer_tools/tools/inspector/tools/inspector_layout.dart
@@ -1,3 +1,5 @@
+import 'package:dragonfly/src/screens/browser/browser_screen.dart';
+import 'package:dragonfly_engine/dragonfly_engine.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -16,7 +18,37 @@ class BoxModelPanel extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      spacing: 8,
       children: [
+        BoxModelLayout(
+          commonStyle: CommonStyle(
+            backgroundColor: null,
+            paddingTop: 2,
+            paddingRight: 2,
+            paddingBottom: 2,
+            paddingLeft: 2,
+            marginTop: 8,
+            marginRight: 8,
+            marginBottom: 8,
+            marginLeft: 8,
+            borderWidth: null,
+            borderLeftColor: null,
+            borderRightColor: null,
+            borderTopColor: null,
+            borderBottomColor: null,
+            borderLeftWidth: 4,
+            borderRightWidth: 4,
+            borderTopWidth: 4,
+            borderBottomWidth: 4,
+            borderRadius: null,
+            maxWidth: 1234,
+            maxHeight: 320,
+            minWidth: null,
+            minHeight: null,
+            isCentered: null,
+            cursor: null,
+          ),
+        ),
         BoxModelProperties(),
       ],
     );
@@ -66,6 +98,128 @@ class _Property extends StatelessWidget {
               style: TextStyle(
                 color: Colors.pink,
               ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class BoxModelLayout extends StatelessWidget {
+  const BoxModelLayout({
+    super.key,
+    required this.commonStyle,
+  });
+
+  final CommonStyle commonStyle;
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTextStyle(
+      style: TextStyle(
+        color: Colors.white,
+        fontWeight: FontWeight.bold,
+      ),
+      child: IdontKnow(
+        color: CommonStyleBlock.marginColor,
+        leftValue: '8',
+        bottomValue: '4',
+        topValue: '4',
+        rightValue: '8',
+        child: IdontKnow(
+          color: Colors.black87,
+          leftValue: '2',
+          bottomValue: '2',
+          topValue: '2',
+          rightValue: '2',
+          child: IdontKnow(
+            color: CommonStyleBlock.paddingColor,
+            leftValue: '8',
+            bottomValue: '4',
+            topValue: '4',
+            rightValue: '8',
+            child: IdontKnow(
+              color: CommonStyleBlock.elementColor,
+              leftValue: null,
+              bottomValue: null,
+              topValue: null,
+              rightValue: null,
+              child: Text(
+                "?x?",
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class IdontKnow extends StatelessWidget {
+  const IdontKnow({
+    super.key,
+    required this.child,
+    required this.color,
+    this.leftValue,
+    this.topValue,
+    this.rightValue,
+    this.bottomValue,
+  });
+
+  final Widget child;
+  final Color color;
+  final String? leftValue;
+  final String? topValue;
+  final String? rightValue;
+  final String? bottomValue;
+
+  @override
+  Widget build(BuildContext context) {
+    const edgeInsets = EdgeInsets.all(4.0);
+
+    return DecoratedBox(
+      decoration: BoxDecoration(color: color),
+      child: Column(
+        children: [
+          Padding(
+            padding: edgeInsets,
+            child: Text(
+              topValue ?? "",
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: edgeInsets,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      leftValue ?? "",
+                      textAlign: TextAlign.center,
+                    ),
+                  ],
+                ),
+              ),
+              Expanded(
+                child: child,
+              ),
+              Padding(
+                padding: edgeInsets,
+                child: Column(
+                    children: [if (rightValue != null) Text(rightValue!)]),
+              ),
+            ],
+          ),
+          Padding(
+            padding: edgeInsets,
+            child: Text(
+              bottomValue ?? "",
+              textAlign: TextAlign.center,
             ),
           ),
         ],
